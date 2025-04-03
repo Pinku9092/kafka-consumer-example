921c304f2a2edb6912aab840ba1b9cefc7239d06
pinku <kpinku32@gmail.com>
2025-04-03 12:09:25 +0530

    added video url

--- src/main/java/com/pinku/KafkaConsumerExampleApplication.java
+++ src/main/java/com/pinku/KafkaConsumerExampleApplication.java
@@ -3,6 +3,8 @@ package com.pinku;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// https://www.youtube.com/watch?v=d0T9g0_G_3U&t=1328s
+
 @SpringBootApplication
 public class KafkaConsumerExampleApplication {
 
